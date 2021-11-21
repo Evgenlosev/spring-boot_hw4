6922ffd3668cf2c0c884d45f90e60e75de247a1e
Evgenlosev <evgenlosev@yandex.ru>
2021-11-21 17:48:17 +0700

    hw4

---
+++ src/main/java/com/geekbrains/spring/web/controllers/MainController.java
@@ -0,0 +1,32 @@
+package com.geekbrains.spring.web.controllers;
+
+import com.geekbrains.spring.web.data.Product;
+import com.geekbrains.spring.web.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MainController {
+    private ProductService productService;
+
+    public MainController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public void deleteProductById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+    @GetMapping("/products/change_cost")
+    public void changeCost(@RequestParam Long productId, @RequestParam Integer delta) {
+        productService.changeCost(productId, delta);
+    }
+}
---
+++ src/main/java/com/geekbrains/spring/web/repositories/ProductRepository.java
@@ -0,0 +1,39 @@
+package com.geekbrains.spring.web.repositories;
+
+import com.geekbrains.spring.web.data.Product;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>(List.of(
+                new Product(1L, "Bread", 40L),
+                new Product(2L, "Apples", 150L),
+                new Product(3L, "Milk", 60L),
+                new Product(4L, "Chocolate", 70L),
+                new Product(5L, "Cheese", 145L)
+        ));
+    }
+
+
+
+    public List<Product> getAllProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public void deleteById(Long id) {
+        products.removeIf(p -> p.getId().equals(id));
+    }
+
+    public Product findById(Long id) {
+        return products.stream().filter(p -> p.getId().equals(id)).findFirst().get();
+    }
+}
